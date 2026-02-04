37b0f6e4c6dbc802de61a0aa77efa61ec347ed41
Michael Schieder <michael.schieder@gmail.com>
2026-02-04 20:25:28 +0100

    added assembly for every jar / optimized scripts

--- idea-code-formatter-lib/src/assembly/minimal-reduced-jars/package_ide.sh
+++ idea-code-formatter-lib/src/assembly/minimal-reduced-jars/package_ide.sh
@@ -8,44 +8,10 @@ echo unzipping minimal idea lib and plugin files
 mkdir -p $IDEA_DIR
 while IFS="" read -r jar || [ -n "$jar" ]
 do
+  # extract the jar from the idea zip file
   unzip -o $DOWNLOAD_FILE "$jar" -d $IDEA_DIR
+  # unzip the jar file to an own target-dir
+  jarfilename=$(basename $jar .jar)
+  mkdir -p target/$jarfilename
+  unzip -o -q "$IDEA_DIR/$jar" -d target/$jarfilename
 done < src/assembly/minimal-jars.txt
-
-# unzip the jars for the assembly plugin
-mkdir -p target/3rd-party-rt
-unzip -o -q "$IDEA_DIR/lib/3rd-party-rt.jar" -d target/3rd-party-rt
-mkdir -p target/util
-unzip -o -q "$IDEA_DIR/lib/util.jar" -d target/util
-mkdir -p target/util-8
-unzip -o -q "$IDEA_DIR/lib/util-8.jar" -d target/util-8
-mkdir -p target/util_rt
-unzip -o -q "$IDEA_DIR/lib/util_rt.jar" -d target/util_rt
-mkdir -p target/app
-unzip -o -q "$IDEA_DIR/lib/app.jar" -d target/app
-mkdir -p target/jps-model
-unzip -o -q "$IDEA_DIR/lib/jps-model.jar" -d target/jps-model
-mkdir -p target/java-impl
-unzip -o -q "$IDEA_DIR/plugins/java/lib/java-impl.jar" -d target/java-impl
-mkdir -p target/stats
-unzip -o -q "$IDEA_DIR/lib/stats.jar" -d target/stats
-mkdir -p target/jps-builders
-unzip -o -q "$IDEA_DIR/plugins/java/lib/jps-builders.jar" -d target/jps-builders
-mkdir -p target/jps-builders-6
-unzip -o -q "$IDEA_DIR/plugins/java/lib/jps-builders-6.jar" -d target/jps-builders-6
-mkdir -p target/protobuf-java6
-unzip -o -q "$IDEA_DIR/plugins/java/lib/rt/protobuf-java6.jar" -d target/protobuf-java6
-
-#copy the original jars to the IDEA_REDUCED_DIR
-cp -r $IDEA_DIR $IDEA_REDUCED_DIR
-
-rm $IDEA_REDUCED_DIR/lib/3rd-party-rt.jar
-rm $IDEA_REDUCED_DIR/lib/util.jar
-rm $IDEA_REDUCED_DIR/lib/util-8.jar
-rm $IDEA_REDUCED_DIR/lib/util_rt.jar
-rm $IDEA_REDUCED_DIR/lib/app.jar
-rm $IDEA_REDUCED_DIR/lib/jps-model.jar
-rm $IDEA_REDUCED_DIR/lib/stats.jar
-rm $IDEA_REDUCED_DIR/plugins/java/lib/java-impl.jar
-rm $IDEA_REDUCED_DIR/plugins/java/lib/jps-builders.jar
-rm $IDEA_REDUCED_DIR/plugins/java/lib/jps-builders-6.jar
-rm $IDEA_REDUCED_DIR/plugins/java/lib/rt/protobuf-java6.jar
