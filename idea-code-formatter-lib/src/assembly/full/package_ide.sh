316e94942a2a12dd803f63e91acc62da2ac08c46
Michael Schieder <michael.schieder@gmail.com>
2026-02-14 00:09:56 +0100

    windows: migrate to git-bash

--- idea-code-formatter-lib/src/assembly/full/package_ide.sh
+++ idea-code-formatter-lib/src/assembly/full/package_ide.sh
@@ -12,8 +12,9 @@ unzip -o $DOWNLOAD_FILE "lib/*/*.jar" -d $IDEA_DIR
 
 echo unzipping idea plugin files
 echo extracting all required plugins
-#while IFS="" read -r plugin || [ -n "$plugin" ]
-while read plugin; do
+while IFS="" read -r plugin || [ -n "$plugin" ]
+do
+#while read plugin; do
   jarfiles=$(echo "plugins/$plugin/**/*.jar"|tr -d '\n'|tr -d '\r')
   echo extracting plugin dir $plugin
   unzip -o $DOWNLOAD_FILE $jarfiles -d $IDEA_DIR
@@ -22,6 +23,7 @@ done < src/assembly/plugin_dirs.txt
 mkdir -p $REPACKAGED_IDEA_DIR
 pushd target
 cp *.jar idea/lib #include also idea-code-formatter-lib
-zip -rq idea.zip idea
+#zip -rq idea.zip idea
+7z a -r idea.zip idea
 popd
 mv target/idea.zip $REPACKAGED_IDEA_DIR
